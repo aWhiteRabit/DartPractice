7e7d9ff81fc0e01a38a441c2fce98c962817dbb0
aWhiteRabit <106929382+aWhiteRabit@users.noreply.github.com>
2022-06-06 04:37:18 +0600

    Update growable_list.dart

--- DartPractice/Assignment_01/List_Array/growable_list.dart
+++ DartPractice/Assignment_01/List_Array/growable_list.dart
@@ -1,3 +1,14 @@
 void main(){
+  var myGrowableList = [];
+  print(myGrowableList);
 
+
+  myGrowableList.add(45);
+  print(myGrowableList);  //added item to the growable list;
+
+  myGrowableList.add(45);
+  print(myGrowableList);  //added item to the growable list;
+
+  myGrowableList.add(45);
+  print(myGrowableList);  //added item to the growable list;
 }
