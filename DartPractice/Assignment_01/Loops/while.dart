24e2f86bc2d5c5dcbdc451d9665c7d866f9acdf9
aWhiteRabit <106929382+aWhiteRabit@users.noreply.github.com>
2022-06-06 04:34:26 +0600

    Update while.dart

--- DartPractice/Assignment_01/Loops/while.dart
+++ DartPractice/Assignment_01/Loops/while.dart
@@ -1,3 +1,10 @@
-void main(){
+void main() {
+  var number = 5;
+  var factorial = 1;
 
+  while(number >=1) {
+    factorial = factorial * number;
+    number--; //5*(5-1)*(4-1)*(3-1)*(2-1)=120
+  }
+  print("The factorial  is ${factorial}");
 }
