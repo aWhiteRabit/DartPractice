9e0dbb8ce6b2539df359f989beaf0e3f70b80ea3
aWhiteRabit <106929382+aWhiteRabit@users.noreply.github.com>
2022-06-06 14:43:40 +0600

    Update returning_function.dart

--- DartPractice/Assignment_01/Functions/returning_function.dart
+++ DartPractice/Assignment_01/Functions/returning_function.dart
@@ -1,5 +1,33 @@
 void main(){
-  
-  will be completed soon;
+
+  print(myFunction()+16);
+
+  List a = ReturnThisFunction();
+  print(a);
 
 }
+
+
+int myFunction(){
+
+  int a = 16;
+  int b = 17;
+
+  int c = (a+b);
+
+  return c;
+}
+
+List ReturnThisFunction(){
+
+  var list = [];
+  list.add("A");
+  list.add("B");
+  list.add("C");
+  list.add("D");
+  list.add("E");
+
+  return list;
+
+}
+
