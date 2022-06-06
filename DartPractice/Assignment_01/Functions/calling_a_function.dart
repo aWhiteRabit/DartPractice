3598b58af6edfc67ec6e1072004f7e2f6cb335ef
aWhiteRabit <106929382+aWhiteRabit@users.noreply.github.com>
2022-06-06 14:12:37 +0600

    Update calling_a_function.dart

--- DartPractice/Assignment_01/Functions/calling_a_function.dart
+++ DartPractice/Assignment_01/Functions/calling_a_function.dart
@@ -1,5 +1,22 @@
 void main(){
-  
-  will be completed soon;
 
+  addTwoNumber();
+  multiplyTwoNumber();
+  
+}
+
+addTwoNumber(){
+  int a = 9;
+  int b = 6;
+  
+  int sum = a + b ;
+  print(sum);
+}
+
+multiplyTwoNumber(){
+  int a = 15;
+  int b = 17;
+
+  int multiply = a * b ;
+  print(multiply);
 }
